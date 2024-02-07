f8bca29c51e7b18e9c2fc6e47cb6899b028c9ef7
ilyas Osman <ilyaso.fiidooow@gmail.comilyaso.fiidooow@gmail.comgit config --global user.email ilyaso.fiidooow@gmail.com>
2024-02-07 14:54:27 -0600

    new

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
-console.log("Hello World!\n==========\n");
-
-
+// Exercise 1: Ignore Even
+console.log("Exercise 1: Ignore Even");
 for (let i = 1; i <= 100; i++) {
     if (i % 2 !== 0) {
         console.log(i);
     }
 }
 
-
-
-
-
-
+// Exercise 2: FIZZBUZZ
+console.log("\nExercise 2: FIZZBUZZ");
 for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
         console.log('FIZZBUZZ');
@@ -22,27 +18,18 @@ for (let i = 1; i <= 100; i++) {
     }
 }
 
-
-let i = 1;
-while (i <= 100) {
-    if (i % 2 !== 0) {
-        console.log(i);
-    }
-    i++;
-}
-
+// Exercise 3: Repeat with While
+console.log("\nExercise 3: Repeat with While");
 let j = 1;
 while (j <= 100) {
-    if (j % 3 === 0 && j % 5 === 0) {
-        console.log('FIZZBUZZ');
-    } else if (j % 3 === 0) {
-        console.log('FIZZ');
-    } else if (j % 5 === 0) {
-        console.log('BUZZ');
+    if (j % 2 !== 0) {
+        console.log(j);
     }
     j++;
 }
 
+// Exercise 3: Repeat with Do/While
+console.log("\nExercise 3: Repeat with Do/While");
 let k = 1;
 do {
     if (k % 2 !== 0) {
@@ -51,64 +38,35 @@ do {
     k++;
 } while (k <= 100);
 
-
-let f = 1;
-do {
-    if (k % 2 !== 0) {
-        console.log(k);
-    }
-    f++;
-} while (f <= 100);
-
-
-
-let y = 1;
-do {
-    if (n % 3 === 0 && n % 5 === 0) {
-        console.log('FIZZBUZZ');
-    } else if (y % 3 === 0) {
-        console.log('FIZZ');
-    } else if (y % 5 === 0) {
-        console.log('BUZZ');
-    }
-    n++;
-} while (n <= 100);
-
-//Exercise 4 Section
-let value = Math.round((Math.random() * 500)); 
-let n = Math.round(Math.random() * (500 - 100) + 100); 
-
-let found = false; 
-
-
-for (let i = 1; i <= n; i++) {
+// Exercise 4: Find Value
+console.log("\nExercise 4: Find Value");
+let value = Math.round(Math.random() * 500); 
+let n = Math.round(Math.random() * (500 - 100) + 100);
+let found = false;
+for (let i = 0; i <= n; i++) {
     if (i === value) {
         console.log("Found value!");
         found = true;
-        break; 
+        break;
     }
 }
-
-
 if (!found) {
     console.log("Did not find value");
 }
 
+// Exercise 5: Customized FIZZBUZZ
+console.log("\nExercise 5: Customized FIZZBUZZ");
 let fizzDivisor = Math.round(Math.random() * (10 - 1) + 1);
 let buzzDivisor = Math.round(Math.random() * (10 - 1) + 1);
-let x = Math.round(Math.random() * (1000 - 1) + 1);
+let m = Math.round(Math.random() * (1000 - 1) + 1);
 let start = Math.round(Math.random() * (10 - 1) + 1);
-
-
-for (let i = start; i <= n; i++) {
-    
-    if (i % fizzDivisor === 0 && i % buzzDivisor === 0) {
-        console.log("FIZZBUZZ");
-    } else if (i % fizzDivisor === 0) {
-        console.log("FIZZ");
-    } else if (i % buzzDivisor === 0) {
-        console.log("BUZZ");
-    } else {
-        console.log(i); 
+for (let i = start; i <= m; i++) {
+    let output = '';
+    if (i % fizzDivisor === 0) {
+        output += 'FIZZ';
     }
+    if (i % buzzDivisor === 0) {
+        output += 'BUZZ';
+    }
+    console.log(output || i);
 }
